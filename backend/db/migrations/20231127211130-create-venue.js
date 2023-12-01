beb82911a12f5b566cdc889d3791b7cb60978e2b
Ahmed Sharfi <ahmedsharfi714@gmail.com>
2023-12-01 16:39:46 -0600

    please work Venue, please

--- backend/db/migrations/20231127211130-create-venue.js
+++ backend/db/migrations/20231127211130-create-venue.js
@@ -36,10 +36,10 @@ module.exports = {
         allowNull: false
       },
       lat: {
-        type: Sequelize.NUMERIC(10, 7)
+        type: Sequelize.DECIMAL(10, 7)
       },
       lng: {
-        type: Sequelize.NUMERIC(10, 7)
+        type: Sequelize.DECIMAL(10, 7)
       },
       createdAt: {
         allowNull: false,
