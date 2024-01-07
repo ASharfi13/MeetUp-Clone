937216c029df6361c6f01cafd8fc5f2aa326ae43
Ahmed Sharfi <ahmedsharfi714@gmail.com>
2024-01-07 16:07:07 -0600

    ID added to Membership Model

--- backend/db/models/membership.js
+++ backend/db/models/membership.js
@@ -14,6 +14,11 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Membership.init({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     userId: {
       type: DataTypes.INTEGER,
       field: "userId"
